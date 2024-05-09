6b4adf0bcba62b49226391bd0254fd1998cffadc
TriPamanto <angga24042001@gmail.com>
2024-05-09 17:09:40 +0700

    controller1

--- routes/web.php
+++ routes/web.php
@@ -110,3 +110,18 @@ Route::delete('/books/{id}', function ($id){
 //     $borrowers = Borrower::all();
 //     return view('borrowers.index', ['borrowers' => $borrowers]);
 // })->name('borrowers.index');
+
+use App\Http\Controllers\BookController;
+
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+
+Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit');
+
+Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
+
+Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
+
