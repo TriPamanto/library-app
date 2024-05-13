1be700d41931a09e4cea1a8b403e10a5a08639cd
TriPamanto <angga24042001@gmail.com>
2024-05-14 00:52:20 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ApiBookController;
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+use App\Http\Controllers\ApiBookController;
 
 Route::get('/books', [ApiBookController::class, 'index']);
 Route::post('/books', [ApiBookController::class, 'store']);
